48f2252ad455122acf151c94827c899fb0b8c469
Paul DeYonghe <p.deyonghe@gmail.com>
2019-03-16 13:10:03 -0500

    got comments, pics, logo working; bookmarking

--- instagram/src/App.js
+++ instagram/src/App.js
@@ -1,22 +1,47 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
 import PostContainer from './Components/PostContainer/PostContainer'
 import SearchBar from './Components/SearchBar/SearchBar'
-import CommentSection from './Components/CommentSection/CommentSection' 
 import dummyData from './dummy-data';
 
-
-
-
-
 class App extends Component {
+  constructor(props) {
+    super(props) 
+    this.state = {
+      posts: [], //posts array placeholder
+      filteredPosts: [] //SearchBar-filtered posts placeholder
+    }
+  }
+
+  componentDidMount() { //lifecycle method, sets posts to all of dummyData
+    this.setState({
+      posts: dummyData,
+    })
+  }
+
+//searchHandler takes argumemt and filters posts the search parameters
+searchHandler = e => {
+  const posts = this.state.posts.filter(p => {
+    if (p.username.includes(e.target.value)){
+      return p
+    }
+  })
+  this.setState({ filteredPosts: posts})
+}
+
+
   render() {
     return (
       <div className="App">
-        <SearchBar />
-        <PostContainer />
-        <CommentSection />
+        <SearchBar 
+          searchTerm={this.state.searchTerm}
+          searchPosts={this.searchHandler} />
+        <PostContainer posts={this.state.filteredPosts.length > 0
+            ? this.state.filteredPosts
+            : this.state.posts
+          }
+        />
+
       </div>
     );
   }
--- instagram/src/Components/PostContainer/PostContainer.js
+++ instagram/src/Components/PostContainer/PostContainer.js
@@ -1,12 +1,22 @@
-import React, { Component } from 'react'
+import React from 'react'
+import Post from './Post'
 import './PostContainer.css'
+import PropTypes from 'prop-types'
 
 const PostContainer = (props) => {
+        return(
+                <div className="posts-containter-wrapper">                
+                        {props.posts.map(post => <Post  key={post.imageUrl} post={post} />)}
+                </div>
+        );
 
-	return(
-		<div>PostContainer Placeholder</div>
-	)
-
+        
 }
 
-export default PostContainer
+        PostContainer.propTypes = {
+                comments: PropTypes.array,
+                imageUrl: PropTypes.string,     
+                likes: PropTypes.number
+        }
+
+export default PostContainer;
