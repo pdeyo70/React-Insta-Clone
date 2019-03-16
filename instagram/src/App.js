40c61aa13cd5e5ad034e68f10586500fa67fb63c
Paul DeYonghe <p.deyonghe@gmail.com>
2019-03-16 12:07:39 -0500

    added dummy-data import

--- instagram/src/App.js
+++ instagram/src/App.js
@@ -2,9 +2,9 @@ import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 import PostContainer from './Components/PostContainer/PostContainer'
-
 import SearchBar from './Components/SearchBar/SearchBar'
 import CommentSection from './Components/CommentSection/CommentSection' 
+import dummyData from './dummy-data';
 
 
 
